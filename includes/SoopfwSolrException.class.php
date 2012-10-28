8513820dcde70a3d1aa114a1a0bfdb1422f4b39f
Christian Ackermann <prdatur@gmail.com>
2012-10-28 23:43:38 +0100

    added Solr Exception

---
+++ includes/SoopfwSolrException.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Provides an exception to determine a solr error.
+ *
+ * @copyright Christian Ackermann (c) 2010 - End of life
+ * @author Christian Ackermann <prdatur@gmail.com>
+ * @category Exceptions
+ */
+class SoopfwSolrException extends SoopfwErrorException
+{
+}
